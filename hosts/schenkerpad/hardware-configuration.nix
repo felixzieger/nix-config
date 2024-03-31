3e2758ef706e89c9c5888eec71ac191c4db759ba
Felix Zieger <github@felixzieger.de>
2024-03-31 11:45:47 +0200

    fix lagging mouse cursor

--- hosts/schenkerpad/hardware-configuration.nix
+++ hosts/schenkerpad/hardware-configuration.nix
@@ -77,6 +77,7 @@
     package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
 
     prime = {
+      sync.enable = true;
       nvidiaBusId = "PCI:1:0:0";
       intelBusId = "PCI:0:2:0";
     };
