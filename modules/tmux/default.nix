0d8803eadcac7ef0ccf316a6650f92e51b9a39f8
Felix Zieger <github@felixzieger.de>
2024-04-05 23:52:25 +0200

    preconfigured tmux sessions

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -1,8 +1,5 @@
 { inputs, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    tmux
-  ];
   programs.tmux = {
     enable = true;
     extraConfig = ''
@@ -33,6 +30,30 @@
     if [ ! -S ~/.ssh/ssh_auth_sock ] && [ -S "$SSH_AUTH_SOCK" ]; then
       ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
     fi
+  '';
+
+  programs.tmux.tmuxinator.enable = true;
+  home.file.".config/tmuxinator/nix-config.yml".text = ''
+    name: nix-config
+    root: /etc/nixos
+
+    # Specifies (by name or index) which window will be selected on project startup. If not set, the first window is used.
+    startup_window: nix
+
+    # Specifies (by index) which pane of the specified window will be selected on project startup. If not set, the first pane is used.
+    # startup_pane: 1
+
+    # Controls whether the tmux session should be attached to automatically. Defaults to true.
+    # attach: false
+
+    windows:
+      - top: btop
+      - nix:
+          layout: even-horizontal
+          panes:
+            - sudo -E nvim -c "NvimTreeOpen"
+            - 
+      - git: sudo -E lazygit
   '';
 }
 
