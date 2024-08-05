808498fae3a213efef7ffde7324649bd952eed05
Felix Zieger <fzieger@meshcloud.io>
2024-08-05 11:37:12 +0200

    open copilot chat vertically

--- hosts/meshpad/fzieger/nvim-copilotchat.lua
+++ hosts/meshpad/fzieger/nvim-copilotchat.lua
@@ -1,13 +1,7 @@
 require('CopilotChat').setup {
-    {
-        window = {
-            layout = 'float',
-            relative = 'cursor',
-            width = 1,
-            height = 0.4,
-            row = 1
-        }
-    },
+    window = {
+        layout = 'vertical',
+    }
 }
 
 -- AI shortcuts start with <leader>a
