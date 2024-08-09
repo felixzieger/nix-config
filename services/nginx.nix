b7229d3af2546414e80d7ea6321081ea26784b53
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-08-09 22:21:51 +0200

    goaccess logs for nginx

--- services/nginx.nix
+++ services/nginx.nix
@@ -1,4 +1,8 @@
-{ pkgs, lib, ... }: {
+{ pkgs, config, lib, ... }:
+let
+  access_logs_root =
+    "/var/www/nginx.${config.networking.hostName}.felixzieger.de";
+in {
   config = {
     networking = {
       firewall = {
@@ -28,5 +32,35 @@
       default = true;
       extraConfig = "deny all;";
     };
+
+    # Nginx Access logs HTML page via GoAccess
+    environment.systemPackages = with pkgs; [ goaccess ];
+
+    systemd.services.goaccess-html = {
+      enable = true;
+      description = "Serves Nginx access logs as HTML file.";
+      path = [ pkgs.goaccess ];
+      script =
+        "goaccess /var/log/nginx/access.log -o ${access_logs_root}/index.html --log-format=COMBINED --real-time-html";
+      serviceConfig = {
+        Type = "simple";
+        User = config.services.nginx.user;
+        Group = config.services.nginx.group;
+      };
+    };
+
+    system.activationScripts.makeWwwDir = lib.stringAfter [ "var" ] ''
+      mkdir -p ${access_logs_root}
+      chown ${config.services.nginx.user}:${config.services.nginx.group} ${access_logs_root}
+    '';
+
+    services.nginx.virtualHosts."nginx.${config.networking.hostName}.felixzieger.de" = # TODO restrict access; e.g. via oauth2-proxy
+      {
+        forceSSL = true;
+        enableACME = true;
+        http3 = true;
+        quic = true;
+        root = access_logs_root;
+      };
   };
 }
