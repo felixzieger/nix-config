743dd8f14bf7f87984b522e7d540065f7cbc941f
Felix Zieger <fzieger@meshcloud.io>
2024-08-20 23:37:57 +0200

    nginx status stub page

--- services/nginx.nix
+++ services/nginx.nix
@@ -26,7 +26,17 @@
 
     services.nginx.virtualHosts."_" = {
       default = true;
-      extraConfig = "deny all;";
+      extraConfig = ''
+        stub_status on;
+        access_log off;
+              
+        allow 127.0.0.1;
+        deny all; 
+      '';
+    };
+
+    services.nginx.virtualHosts."127.0.0.1" = {
+      extraConfig = "stub_status on;";
     };
   };
 }
