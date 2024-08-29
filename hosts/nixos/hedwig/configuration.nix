561257ae2e42324c2a28fc42d3fe228c61f2bf53
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-08-29 22:26:04 +0000

    clean up hedwig

--- hosts/nixos/hedwig/configuration.nix
+++ hosts/nixos/hedwig/configuration.nix
@@ -7,11 +7,6 @@
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
   networking.hostName = "hedwig";
-  networking.domain = "";
-  services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKZYRUQMRILMUlmxyONcKMrHR6VA6R7tYueaF2dCNuOy xilef@meshpad"
-  ];
   system.stateVersion = "23.11";
   programs.tmux = {
     enable = true;
