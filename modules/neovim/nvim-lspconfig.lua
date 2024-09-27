54b138c2f8e333d39ceabd91d5e4eb5d8b2ab88f
Felix Zieger <fzieger@meshcloud.io>
2024-09-27 11:46:51 +0200

    remove lsp binding that conflicts with telescope binding

--- modules/neovim/nvim-lspconfig.lua
+++ modules/neovim/nvim-lspconfig.lua
@@ -47,7 +47,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
 
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- Makes <leader>r slow to appear
     vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
   end
 })
