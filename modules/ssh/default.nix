03283e86f6c4ef4fa5696672eeeee3830c4b38c0
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-10-23 18:06:07 +0200

    explain why I manage file manually

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,5 +1,8 @@
 { ... }: {
   # eternal terminal requires host config
+  # https://mynixos.com/home-manager/option/programs.ssh.matchBlocks and
+  # https://mynixos.com/home-manager/option/programs.ssh.extraConfig
+  # didn't work, so I manage the file manually
     home.file.".ssh/config".text = ''
     Host blausieb
         HostName blausieb.felixzieger.de
