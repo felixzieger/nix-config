6367704a83dd57365cda0890b765bb6dc3721787
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-10-28 22:56:26 +0100

    activate lsps for cmp

--- hosts/macbook/nvim-lspconfig.lua
+++ hosts/macbook/nvim-lspconfig.lua
@@ -1,5 +1,6 @@
 local lspconfig = require('lspconfig')
-lspconfig.terraformls.setup {}
-lspconfig.html.setup {}
-lspconfig.pyright.setup {}
-lspconfig.ruff_lsp.setup {}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+lspconfig.terraformls.setup { capabilities = capabilities }
+lspconfig.html.setup { capabilities = capabilities }
+lspconfig.pyright.setup { capabilities = capabilities }
+lspconfig.ruff_lsp.setup { capabilities = capabilities }
