6fe6320dcbcf07876411df36ad538dd4c586d9dc
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-10-31 15:06:12 +0100

    fish aliases for navigation and docsy cli

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -12,7 +12,12 @@
       tree = "ls --tree";
       dog = "doggo"; # doesn't work. don't know why
       dig = "dog";
+
       docsy = "cd ~/Documents/docsy/cli && bun run docsy";
+
+      dcli = "cd ~/Documents/docsy/cli";
+      ddocs = "cd ~/Documents/docsy/docs";
+      dlz = "cd ~/Documents/docsy/landing-page";
     };
     shellAbbrs = { unset = "set --erase"; };
   };
