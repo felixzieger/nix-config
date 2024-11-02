ad162b57c4fc463a0b4cbd3334fda10498d65c6b
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-02 20:34:27 +0100

    sensible tmux window names

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -1,7 +1,20 @@
-{ ... }: {
+{ pkgs, ... }:
+let
+  tmux-window-name = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tmux-window-name";
+    version = "head";
+    src = pkgs.fetchFromGitHub {
+      owner = "bcotton";
+      repo = "tmux-window-name";
+      rev = "0bb0148623782dbfb5c15741111f0402609f516f";
+      sha256 = "sha256-xb0GGBZ4Ox3LQjKZJ8MzJluElxRJm3BB73F2CMFJEa0=";
+    };
+  };
+in {
   programs.tmux = {
     enable = true;
     extraConfig = builtins.readFile ./tmux.conf;
+    plugins = [{ plugin = tmux-window-name; }];
   };
 
   programs.tmux.tmuxinator.enable = true;
