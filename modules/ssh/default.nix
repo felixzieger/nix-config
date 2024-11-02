de91ddc1c89b2f83e226e0da0e393544baafc621
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-02 18:13:43 +0100

    disable hostkey checking

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -4,19 +4,20 @@
   # https://mynixos.com/home-manager/option/programs.ssh.extraConfig
   # didn't work, so I manage the file manually
     home.file.".ssh/config".text = ''
+    Host *
+        StrictHostKeyChecking no
+        ForwardAgent yes
+
     Host blausieb
         HostName blausieb.felixzieger.de
         Port 33111
-        ForwardAgent yes
 
     Host schwalbe
         HostName schwalbe.felixzieger.de
         Port 33111
-        ForwardAgent yes
 
     Host sonnenhofserver
         HostName nextcloud.sonnenhof-zieger.de
         Port 33111
-        ForwardAgent yes
     '';
 }
