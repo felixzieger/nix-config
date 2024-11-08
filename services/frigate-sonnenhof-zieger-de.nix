4b5d84fe736886da09971e0ac49430168f703a18
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-08 19:57:48 +0100

    use low res stream for detection, use high res for recording

--- services/frigate-sonnenhof-zieger-de.nix
+++ services/frigate-sonnenhof-zieger-de.nix
@@ -87,19 +87,28 @@ in {
         snapshots.enabled = true;
 
         cameras."kartoffelbox" = {
-          ffmpeg.inputs = [{
-            # It's ugly to have the camera credential checked into git.
-            # The service implementation stores the frigate.yml in nix store and I couldn't figure out how to 
-            # get the path of that file to do the Agenix "Replace inplace strings with secrets" trick.
-            # While this would be an improvement, the nix store wouldn't be ideal since it's world readable.
-            # Since the camera is only reachable from within the private network and RTSP is not encrypted anyway I decided to leave it like that.
-            path =
-              "rtsp://BJA3Y0v5:gZbBIUBXeyj4w70q@192.168.178.131:1337/live/ch1";
-            # ch0 is FHD. It stopeed working after 12 hours of streaming. Thus switched to lower resolution ch1
-            # path = "rtsp://BJA3Y0v5:gZbBIUBXeyj4w70q@192.168.178.131:1337/live/ch0";
-            input_args = "preset-rtsp-restream";
-            roles = [ "record" ];
-          }];
+          # It's ugly to have the camera credential checked into git.
+          # The service implementation stores the frigate.yml in nix store and I couldn't figure out how to 
+          # get the path of that file to do the Agenix "Replace inplace strings with secrets" trick.
+          # While this would be an improvement, the nix store wouldn't be ideal since it's world readable.
+          # Since the camera is only reachable from within the private network and RTSP is not encrypted anyway I decided to leave it like that.
+          ffmpeg.inputs = [
+            {
+              path =
+                # ch1 is lower resolution
+                "rtsp://BJA3Y0v5:gZbBIUBXeyj4w70q@192.168.178.131:1337/live/ch1";
+              input_args = "preset-rtsp-restream";
+              roles = [ "detect" ];
+            }
+
+            {
+              path =
+                # ch0 is FHD            
+                "rtsp://BJA3Y0v5:gZbBIUBXeyj4w70q@192.168.178.131:1337/live/ch0";
+              input_args = "preset-rtsp-restream";
+              roles = [ "record" ];
+            }
+          ];
         };
 
         telemetry = { version_check = false; };
