c9868daaeec0f8b85a6170ec6611f89497de96fb
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-10 13:21:30 +0100

    cameron swap

--- hosts/nixos/cameron/hardware-configuration.nix
+++ hosts/nixos/cameron/hardware-configuration.nix
@@ -41,7 +41,10 @@
     fsType = "vfat";
   };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 1 * 1024;
+  }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
