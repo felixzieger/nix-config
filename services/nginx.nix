828d4495f6bad4de8a0458dcef46ccd1638b6915
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-19 15:43:18 +0100

    better rejection for nginx

--- services/nginx.nix
+++ services/nginx.nix
@@ -24,14 +24,22 @@
       recommendedTlsSettings = true;
     };
 
+    # We force users to use proper server names
     services.nginx.virtualHosts."_" = {
       default = true;
+      rejectSSL = true;
       extraConfig = ''
         deny all; 
       '';
     };
     services.fail2ban = {
       jails = {
+        ngnix-botsearch.settings = {
+          enabled = true;
+          filter = "nginx-botsearch";
+          maxretry = 5;
+          findtime = 3600;
+        };
         ngnix-url-probe.settings = {
           enabled = true;
           filter = "nginx-url-probe";
@@ -42,16 +50,15 @@
           backend =
             "auto"; # Do not forget to specify this if your jail uses a log file
           maxretry = 5;
-          findtime = 600;
+          findtime = 3600;
         };
       };
     };
     environment.etc = {
-      # Defines a filter that detects URL probing by reading the Nginx access log
       "fail2ban/filter.d/nginx-url-probe.local".text = pkgs.lib.mkDefault
         (pkgs.lib.mkAfter ''
           [Definition]
-          failregex = ^<HOST>.*(GET /(wp-|admin|boaform|phpmyadmin|\.env|\.git)|\.(dll|so|cfm|asp)|(\?|&)(=PHPB8B5F2A0-3C92-11d3-A3A9-4C7B08C10000|=PHPE9568F36-D428-11d2-A769-00AA001ACF42|=PHPE9568F35-D428-11d2-A769-00AA001ACF42|=PHPE9568F34-D428-11d2-A769-00AA001ACF42)|\\x[0-9a-zA-Z]{2})
+          failregex = ^<HOST> - .* "(GET|POST) /(wp-|admin|boaform|phpmyadmin|\.env|\.git|.*\.(dll|so|cfm|asp)) HTTP/.*" 4[0-9]{2} .*
         '');
     };
   };
