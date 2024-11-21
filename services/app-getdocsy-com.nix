a134021441addf86c2b7dbbe60d6055d62214472
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-21 16:29:07 +0100

    update docsy

--- services/app-getdocsy-com.nix
+++ services/app-getdocsy-com.nix
@@ -6,7 +6,7 @@ let
   docsyDataDir = "/data/docsy/data";
   docsyVersion = "v0.7.8";
   docsyWebDataDir = "/data/docsy_web/data";
-  docsyWebVersion = "v0.0.10";
+  docsyWebVersion = "v0.0.12";
 in {
   config = {
     # Inspect sqlite database without docker exec
