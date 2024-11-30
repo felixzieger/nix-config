1e719b76d2b5e472bf361f76fef115c0690f7783
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-30 12:39:55 +0000

    longer max bantime

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -72,8 +72,8 @@
     bantime = "24h"; # Ban IPs for one day on the first ban
     bantime-increment = {
       enable = true; # Enable increment of bantime after each violation
-      multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h"; # Do not ban for more than 1 week
+      multipliers = "1 2 4 8 16 32 64 128";
+      maxtime = "1680h"; # Do not ban for more than 10 week
       overalljails = true; # Calculate the bantime based on all the violations
     };
     ignoreIP = [
