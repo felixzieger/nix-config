6d6e7ccba549f1e52288e7544da285defdfa671f
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-12-20 22:20:50 +0100

    bitcoin onion port

--- services/bitcoin.nix
+++ services/bitcoin.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   # stored under /etc/nix-bitcoin-secrets
   nix-bitcoin.generateSecrets = true;
@@ -18,4 +18,7 @@
     dataDir = "/data/bitcoind";
     dbCache = 4096;
   };
+
+  # Open the p2p port in the firewall
+  networking.firewall.allowedTCPPorts = [ config.services.bitcoind.onionPort ];
 }
