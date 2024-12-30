960f40ac149693d08afa5ea25c2549ae293e67d7
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-12-30 23:39:14 +0100

    rm unused fish aliases

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -10,16 +10,8 @@
       ll = "ls -l";
       la = "ls -la";
       tree = "ls --tree";
-      dog = "doggo"; # doesn't work. don't know why
       dig = "dog";
-
-      docsy = "cd ~/Documents/docsy/cli && bun run docsy";
-
-      dcli = "cd ~/Documents/docsy/cli";
-      ddocs = "cd ~/Documents/docsy/docs";
-      dlz = "cd ~/Documents/docsy/landing-page";
     };
-    shellAbbrs = { unset = "set --erase"; };
   };
   programs.zoxide.enable = true;
 }
