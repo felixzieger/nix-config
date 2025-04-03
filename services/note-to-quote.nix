3f152f712462b3ed6caf78d711877783ce8cdb1a
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-04-03 12:10:48 +0200

    exclude note to quote from automatic updates

--- services/note-to-quote.nix
+++ services/note-to-quote.nix
@@ -1,7 +1,8 @@
 { config, ... }:
 let
   noteToQuoteVersion = "v0.10";
-in {
+in
+{
   age.secrets = {
     ghcr-secret.file = ../secrets/ghcr-secret.age;
     note-to-quote-env.file = ../secrets/note-to-quote-env.age;
@@ -17,6 +18,9 @@ in {
           passwordFile = config.age.secrets.ghcr-secret.path;
         };
         environmentFiles = [ config.age.secrets.note-to-quote-env.path ];
+        labels = {
+          "com.centurylinklabs.watchtower.enable" = "false";
+        }; # Private registry pulls fail for my watchtower config. Don't need them anyway right now.
       };
     };
   };
