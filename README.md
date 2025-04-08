d339a5b6e34a1213aaed398cbf84b7038cc6c54c
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-04-08 23:00:43 +0200

    btcpay server for sonnenhof

--- README.md
+++ README.md
@@ -79,7 +79,7 @@ restic-uptime-kuma restore latest --target /
 
 # Up next
 
-- Radicle (git hosting) on Blausieb
+- BTCPay Server backup (incl postgres)
 - Impermanence https://lantian.pub/en/article/modify-computer/nixos-impermanence.lantian/
 - Nextcloud backup to Schwalbe
 - Get rid of ../../ imports. Have a look at https://github.com/NotAShelf/nyx/
--- services/bitcoin.nix
+++ services/bitcoin.nix
@@ -19,6 +19,19 @@
     dbCache = 4096;
   };
 
+  services.nginx.virtualHosts."pay.sonnenhof-zieger.de" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.btcpayserver.port}";
+    };
+  };
+
+  services.btcpayserver = {
+    enable = true;
+    dataDir = "/data/btcpayserver";
+  };
+
   # Open the p2p port in the firewall
   networking.firewall.allowedTCPPorts = [ config.services.bitcoind.onionPort ];
 }
