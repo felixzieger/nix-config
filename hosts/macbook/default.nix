8e386077da292b2158cba5e92c1b96af70ddb331
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-04-24 15:12:21 +0200

    rm signal

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -179,7 +179,7 @@ in
     pkgs.rectangle
     pkgs.monitorcontrol
     pkgs.spotify
-    unstable.signal-desktop # Receives a lot of updates, better stay with the newest one we can
+    # unstable.signal-desktop # not available on darwin as of 2025-4-22
     # unstable.whatsapp-for-mac # Too often outdated
     # pkgs.thunderbird # Not supported for x86_64-apple-darwin as of 2024-01-22
     # pkgs.firefox # Not supported for x86_64-apple-darwin as of 2024-01-22
