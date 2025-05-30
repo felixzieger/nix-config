dcedab52a6a04658d585a11828d06d9241630f6a
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-05-30 23:38:43 +0200

    fix zsh init

--- modules/fzf/default.nix
+++ modules/fzf/default.nix
@@ -11,6 +11,7 @@
     if [ -n "''${commands[fzf-share]}" ]; then
       source "$(fzf-share)/key-bindings.zsh"
       source "$(fzf-share)/completion.zsh"
+    fi
   '';
 
   programs.bash.initExtra = ''
