ab5709524183079dc33b671690ef75146ef43f93
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-06-15 16:36:16 +0200

    hints for lsp keybindings

--- modules/neovim/nvim-which-key.lua
+++ modules/neovim/nvim-which-key.lua
@@ -1,1 +1,22 @@
-require("which-key").setup {}
+local wk = require("which-key")
+
+wk.setup {}
+
+-- Register LSP keybindings with descriptions
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('WhichKeyLspConfig', {}),
+  callback = function(ev)
+    wk.register({
+      g = {
+        name = "LSP/Go to",
+        h = { "Hover" },
+        j = { "Signature Help" },
+        i = { "Implementation" },
+        d = { "Type Definition" },
+        f = { "Declaration" },
+        g = { "Definition" },
+        r = { "References" },
+      }
+    }, { prefix = "<leader>", buffer = ev.buf })
+  end
+})
