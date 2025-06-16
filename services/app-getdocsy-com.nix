53b431941bcc50123c8a49ca00b21aa36ea350dc
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-06-16 20:47:30 +0200

    bump docsy version

--- services/app-getdocsy-com.nix
+++ services/app-getdocsy-com.nix
@@ -6,7 +6,7 @@ let
   docsyDataDir = "/data/docsy/data";
   docsyVersion = "v0.7.16";
   docsyWebDataDir = "/data/docsy_web/data";
-  docsyWebVersion = "v0.0.75";
+  docsyWebVersion = "v0.0.76";
 in
 {
   config = {
