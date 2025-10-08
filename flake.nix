c682621b39b6eb71797c18f2926b6a85746f9f57
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-10-08 20:11:35 +0200

    disable twenty

--- flake.nix
+++ flake.nix
@@ -126,7 +126,6 @@
             ./services/app-getdocsy-com.nix
             ./services/plausible-sonnenhof-zieger-de
             ./services/bitwarden-sonnenhof-zieger-de.nix
-            ./services/twenty-getdocsy-com.nix
             ./services/paperless-sonnenhof-zieger-de.nix
             ./services/readeck-felixzieger-de.nix
             ./services/tailscale.nix
