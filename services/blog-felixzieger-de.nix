91692a66fee2836485193eac6c9666e545057fd4
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-11-03 23:59:12 +0100

    move ghost to mysql

--- services/blog-felixzieger-de.nix
+++ services/blog-felixzieger-de.nix
@@ -3,6 +3,7 @@ let
   ghostHost = "blog.felixzieger.de";
   ghostPort = 2368;
   ghostDataDir = "/data/blog-felixzieger-de/data";
+  ghostDbDir = "/data/blog-felixzieger-de/mysql";
 in
 {
   config = {
@@ -22,6 +23,20 @@ in
 
     virtualisation.oci-containers = {
       containers = {
+        ghost-mysql = {
+          autoStart = true;
+          image = "mysql:8.4.7";
+          volumes = [
+            "${ghostDbDir}:/var/lib/mysql"
+          ];
+          environment = {
+            MYSQL_DATABASE = "ghost";
+            MYSQL_USER = "ghost";
+          };
+          environmentFiles = [ config.age.secrets.blog-felixzieger-de-environment.path ];
+          extraOptions = [ "--network=ghost-bridge" ];
+        };
+
         ghost = {
           autoStart = true;
           image = "ghost:5-alpine";
@@ -30,14 +45,17 @@ in
             "/data/ghost/logs:/var/lib/ghost/content/logs"
           ];
           ports = [ "${builtins.toString ghostPort}:${builtins.toString ghostPort}" ];
+          dependsOn = [ "ghost-mysql" ];
           environment = {
             NODE_ENV = "production";
             url = "https://${ghostHost}";
 
             server__port = toString ghostPort;
 
-            database__client = "sqlite3";
-            database__connection__filename = "/var/lib/ghost/content/data/ghost.db";
+            database__client = "mysql";
+            database__connection__host = "ghost-mysql";
+            database__connection__user = "ghost";
+            database__connection__database = "ghost";
 
             mail__from = "noreply@blog.felixzieger.de";
             mail__transport = "SMTP";
@@ -54,6 +72,7 @@ in
             logging__path = "/var/lib/ghost/content/logs";
           };
           environmentFiles = [ config.age.secrets.blog-felixzieger-de-environment.path ];
+          extraOptions = [ "--network=ghost-bridge" ];
         };
       };
     };
@@ -67,7 +86,10 @@ in
       ghost = {
         initialize = true;
 
-        paths = [ ghostDataDir ];
+        paths = [
+          ghostDataDir
+          ghostDbDir
+        ];
 
         repository = "b2:blog-felixzieger-de";
         environmentFile = config.age.secrets.blog-felixzieger-de-restic-environment.path;
@@ -85,5 +107,26 @@ in
         ];
       };
     };
+
+    systemd.services.init-ghost-network = {
+      description = "Create the network ghost-bridge for Ghost.";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig.Type = "oneshot";
+      script =
+        let
+          dockercli = "${config.virtualisation.docker.package}/bin/docker";
+        in
+        ''
+          check=$(${dockercli} network ls | grep "ghost-bridge" || true)
+          if [ -z "$check" ]; then
+            ${dockercli} network create ghost-bridge
+          else
+            echo "ghost-bridge already exists in docker"
+          fi
+        '';
+    };
   };
 }
+
